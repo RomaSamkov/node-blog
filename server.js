2ead49624d0f49c9342638a2ac51d5ab9cc455e3
RomaSamkov <Romansamkov.rs@gmail.com>
2022-09-06 17:46:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+require('dotenv').config();
+
+const app = express();
+
+const { router } = require('');
+
+const PORT = process.env.PORT || 8081;
+
+app.use(express.json());
+app.use(morgan('tiny'));
+
+app.listen(PORT, error => {
+  if (error) {
+    console.log('error at server luunch:', error);
+  }
+  console.log(`Server run on port ${PORT}`);
+});
