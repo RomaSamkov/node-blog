bed4b2f307afaa9d004d99c7e8e5d3574598a29d
RomaSamkov <Romansamkov.rs@gmail.com>
2022-09-06 19:42:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ require('dotenv').config();
 
 const app = express();
 
-const { router } = require('');
+const { postsRouter } = require('./routers/postsRouter');
 
 const PORT = process.env.PORT || 8081;
 
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use('/api/posts', postsRouter);
 
 app.listen(PORT, error => {
   if (error) {
